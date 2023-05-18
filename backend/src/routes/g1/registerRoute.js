0dc1b8e2345959aa48d3c99b91653033513d5df5
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-05-18 12:12:29 +0700

    :)

--- backend/src/routes/g1/registerRoute.js
+++ backend/src/routes/g1/registerRoute.js
@@ -5,82 +5,82 @@ const groupOneRouter = express.Router();
 const app = express();
 app.use(bodyParser.json({ type: "application/json" }));
 
-groupOneRouter.get("/getRecentPlaylists", (req, res) => {
-  const user_id = req.body.user_id;
-  var sql = mysql.format(
-    "SELECT * FROM playlists JOIN user_recent_playlists ON playlists.id = user_recent_playlists.playlist_id WHERE user_recent_playlists.user_id = '?' ",
-    [user_id]
-  );
-  connection.query(sql, [user_id], (err, rows) => {
-    if (err) {
-      return res.json({
-        success: false,
-        error: err,
-      });
-    } else {
-      return res.json({
-        success: true,
-        data: rows,
-      });
-    }
-  });
+groupOneRouter.post("/getRecentPlaylists", (req, res) => {
+    const user_id = req.body.params.user_id;
+    var sql = mysql.format(
+        "SELECT * FROM playlists JOIN user_recent_playlists ON playlists.id = user_recent_playlists.playlist_id WHERE user_recent_playlists.user_id = '?' ",
+        [user_id]
+    );
+    connection.query(sql, [user_id], (err, rows) => {
+        if (err) {
+            return res.json({
+                success: false,
+                error: err,
+            });
+        } else {
+            return res.json({
+                success: true,
+                data: rows,
+            });
+        }
+    });
 });
 
 groupOneRouter.get("/getPlaylists", (req, res) => {
-  const user_id = req.body.user_id;
-  var sql = mysql.format(
-    "SELECT * FROM playlists JOIN user_recent_playlists ON playlists.id = user_recent_playlists.playlist_id WHERE user_recent_playlists.user_id = '?' ",
-    [user_id]
-  );
-  connection.query(sql, [user_id], (err, rows) => {
-    if (err) {
-      return res.json({
-        success: false,
-        error: err,
-      });
-    } else {
-      return res.json({
-        success: true,
-        data: rows,
-      });
-    }
-  });
+    const user_id = req.body.user_id;
+    var sql = mysql.format(
+        "SELECT * FROM playlists JOIN user_recent_playlists ON playlists.id = user_recent_playlists.playlist_id WHERE user_recent_playlists.user_id = '?' ",
+        [user_id]
+    );
+    connection.query(sql, [user_id], (err, rows) => {
+        if (err) {
+            return res.json({
+                success: false,
+                error: err,
+            });
+        } else {
+            return res.json({
+                success: true,
+                data: rows,
+            });
+        }
+    });
 });
 
 groupOneRouter.get("/getData", (req, res) => {
-  var sql = mysql.format("SELECT * FROM user_recent_playlists");
-  connection.query(sql, (err, rows) => {
-    if (err) {
-      return res.json({
-        success: false,
-      });
-    } else {
-      return res.json({
-        success: true,
-        data: rows,
-      });
-    }
-  });
+    var sql = mysql.format("SELECT * FROM user_recent_playlists");
+    connection.query(sql, (err, rows) => {
+        if (err) {
+            return res.json({
+                success: false,
+            });
+        } else {
+            return res.json({
+                success: true,
+                data: rows,
+            });
+        }
+    });
 });
 
 groupOneRouter.get("/getFeedItems", (req, res) => {
-  const user_id = req.body.user_id;
-  var sql = mysql.format("SELECT * FROM feed_kinds WHERE user_id = '?'", [
-    user_id,
-  ]);
-  connection.query(sql, (err, rows) => {
-    if (err) {
-      return res.json({
-        success: false,
-      });
-    } else {
-      return res.json({
-        success: true,
-        data: rows,
-      });
-    }
-  });
+    const user_id = req.body.user_id;
+    var sql = mysql.format("SELECT * FROM feed_kinds WHERE user_id = '?'", [
+        user_id,
+    ]);
+    connection.query(sql, (err, rows) => {
+        if (err) {
+            return res.json({
+                success: false,
+            });
+        } else {
+            return res.json({
+                success: true,
+                data: rows,
+            });
+        }
+    });
 });
 export default function registerGroupOne(app) {
-  app.use("/groupOne", groupOneRouter);
+    app.use("/groupOne", groupOneRouter);
 }
