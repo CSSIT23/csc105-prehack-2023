82c6b532ef265e7c38756e50d5ff3b7ab9e6d023
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-05-18 11:47:22 +0700

    change

--- backend/src/routes/g15/registerRoute.js
+++ backend/src/routes/g15/registerRoute.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const mysql = require("mysql2");
+// const express = require("express");
+// const mysql = require("mysql2");
 
-const groupFifteenRouter = express.Router();
+// const groupFifteenRouter = express.Router();
 
-groupFifteenRouter.get("/track/:trackId", (req, res) => {
-  res.send("Hello World");
-});
+// groupFifteenRouter.get("/track/:trackId", (req, res) => {
+//     res.send("Hello World");
+// });
 
-export default function registerGroupFifteen(app) {
-  app.use("/groupFifteen", groupFifteenRouter);
-}
+// export default function registerGroupFifteen(app) {
+//     app.use("/groupFifteen", groupFifteenRouter);
+// }
