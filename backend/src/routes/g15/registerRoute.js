026acf0d96ebd5ac6b45f8444f73d4b5edfffae8
xXNeonKitsuneXx <xXNeonKitsuneXx@gmail.com>
2023-05-18 11:23:39 +0700

    aaa

--- backend/src/routes/g15/registerRoute.js
+++ backend/src/routes/g15/registerRoute.js
@@ -5,7 +5,7 @@ const groupFifteenRouter = express.Router();
 
 groupFifteenRouter.get("/track/:trackId", (req, res) => {
   const { trackId }  = req.params
-
+  const Neo = 0
 
   const query = connection.query('SELECT lyric FROM tracks, track_lyrics WHERE tracks.id = ? and tracks.id = track_lyrics.track_id order by `order`', [trackId], (err, rows) => {
     if (err) {
@@ -21,9 +21,6 @@ groupFifteenRouter.get("/track/:trackId", (req, res) => {
         data: rows,
         error: null,
       });
-  
-
-
     }
   });
 });
