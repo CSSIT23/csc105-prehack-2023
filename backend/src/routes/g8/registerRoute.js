046e30838ce8dd13f1b36b135a66bf0181558e53
opnxjm <onparin.jitm@mail.kmutt.ac.th>
2023-05-18 11:14:48 +0700

    finish

--- backend/src/routes/g8/registerRoute.js
+++ backend/src/routes/g8/registerRoute.js
@@ -2,8 +2,20 @@ import express from "express";
 
 const groupEightRouter = express.Router();
 
-groupEightRouter.get("/", (req, res) => {
-  res.send("Hello World");
+groupEightRouter.get("/album/:albumId", async (req, res) => {
+  const albumId = req.params.albumId;
+  connection.query(
+    "SELECT albums.id as album_id, albums.name AS album_name, albums.year, tracks.id as track_id, tracks.name AS track_name, tracks.duration, artists.name AS artist_name, SUM(tracks.duration) AS total_duration FROM albums INNER JOIN tracks ON albums.id = tracks.album_id INNER JOIN track_artists ON tracks.id = track_artists.track_id INNER JOIN artists ON track_artists.artist_id = artists.id WHERE albums.id = ?", [albumId], (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error retrieving tracks record");
+      } else if (result.length === 0) {
+        res.status(404).send("Track not found");
+      } else {
+
+        res.send(result);
+      }
+    })
 });
 
 export default function registerGroupEight(app) {
