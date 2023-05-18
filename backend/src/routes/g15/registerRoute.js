55fd1451aa9898b2230c40434fb0d8cb260222ec
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2023-05-18 12:07:17 +0700

    AAAAAA

--- backend/src/routes/g15/registerRoute.js
+++ backend/src/routes/g15/registerRoute.js
@@ -5,9 +5,39 @@ import express from "express"
 const groupFifteenRouter = express.Router();
 
 groupFifteenRouter.get("/track/:trackId", (req, res) => {
+<<<<<<< Updated upstream
   res.send("Hello World");
 });
 
+=======
+  const { trackId }  = req.params
+
+  const query = connection.query('SELECT lyric FROM tracks, track_lyrics WHERE tracks.id = ? and tracks.id = track_lyrics.track_id order by `order`', [trackId], (err, rows) => {
+      // Return data to the client if success
+      return res.json({
+        success: true,
+        data: rows,
+        error: null,
+      });
+    
+  });
+});
+
+groupFifteenRouter.get("/artist/:artistId",(req, res)=>{
+  const { artistId } = req.params
+
+  const query = connection.query('SELECT id, name, profile_url FROM artists WHERE artists.id = ? and artists.name = ? and artists.profile_url = ?',[artistId], (err,rows)=>{
+    return res.json({
+      success: true,
+      data: rows,
+      error: null,
+    });
+  })
+});
+
+///////////////////////////////////////////////////////
+>>>>>>> Stashed changes
 export default function registerGroupFifteen(app) {
   app.use("/groupFifteen", groupFifteenRouter);
 }
+
